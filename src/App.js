e48a48b42d31a320fc6625ef4b875424244e2596
Chris <sjshin0318@gmail.com>
2023-09-29 12:43:04 +0900

    item에 배열 추가

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 
 function App() {
     const name = 'Chris';
+    const list = ['우유', '딸기', '바나나', '요거트'];
     return (
         <>
-            <h1 className='orange'>Hello!</h1>
+            <h1 className='orange'>{`Helllo! ${name}`}</h1>
             <h2>Hello!</h2>
             <p>{name}</p>
             <ul>
-                <li>우유</li>
-                <li>딸기</li>
-                <li>바나나</li>
+                { list.map((item) => (
+                    <li>{item}</li>
+                ))}
             </ul>
             <img style={{width: '200px', height: '200px'}}
                  src='https://images.unsplash.com/photo-1695755594813-14e57fab9a56?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80'
