e84964f40b8df1518f76017bc2a459ccef0bb97e
Chris <sjshin0318@gmail.com>
2023-09-30 16:24:26 +0900

    Counter component 추가

---
+++ src/AppCounter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Counter from './components/Counter'
+export default function AppCounter() {
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
+}
--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -5,8 +5,16 @@ import Avatar from "./components/Avatar"; //자동으로 import 됨.
 function AppProfile() {
     const name = 'Chris';
     const list = ['우유', '딸기', '바나나', '요거트'];
+    const handleClick =(event) => {
+        console.log(event);
+        alert("버튼이 클릭됨!");
+    };
     return (
         <>
+            <button onClick={handleClick}>버튼</button>
+            <form onSubmit={() => {}}>
+                <input />
+            </form>
             <Avatar
                 image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
                 isNew={true}
