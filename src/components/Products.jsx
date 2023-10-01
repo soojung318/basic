3eac2c6c46e17186c5a5f9d951d0bf9323cb4953
Chris <sjshin0318@gmail.com>
2023-10-01 20:13:55 +0900

    Products id 추가

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 
 export default function Products() {
-    const [count, setCount] = useState(0);
     const [products, setProducts] = useState([]);
+    const [checked, setChecked] = useState(false);
+    const handleChange = () => setChecked((prev) => !prev);
 
     // fetch(비동기 통신)만 사용하면 무한루프 돈다.
     // fetch('data/products.json')
@@ -14,7 +15,7 @@ export default function Products() {
 
     // useEffect 함수는 컴포넌트가 화면에 보여질 때 처음 한 번만 실행한다.
     useEffect(() => {
-        fetch('data/products.json')
+        fetch(`data/${checked ? 'sale_':''}products.json`)
             .then((res) => res.json())
             .then((data) => {
                 console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
@@ -23,12 +24,14 @@ export default function Products() {
         return () => {
             console.log('🧹깨끗하게 청소하는 일들을 합니다.');
         };
-    }, []);
+    }, [checked]);
     return (
         <>
+            <input id="checkbox" type="checkbox" value={checked} onChange={handleChange} />
+            <label htmlFor='checkbox'>Show Only 🔥 Sale</label>
             <ul>
                 {products.map((product) => (
-                    <li key={product.id}>
+                    <li key={product.id }>
                         <article>
                             <h3>{product.name}</h3>
                             <p>{product.price}</p>
@@ -36,7 +39,6 @@ export default function Products() {
                     </li>
                 ))}
             </ul>
-            <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
         </>
     );
 }
